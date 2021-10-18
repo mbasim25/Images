e10f7bd717950576ed012e3697ce8cc7a5bc14dd
mbasim25 <mohammedbasim2525@gmail.com>
2021-10-18 23:06:59 +0300

    Added: pagination validator

--- src/routers/images.ts
+++ src/routers/images.ts
@@ -2,31 +2,31 @@ import { Request, Response, Router } from "express";
 import { images } from "../controllers/";
 import { upload } from "../utils/";
 import { validateRequest } from "../middlewares";
-import { validateId } from "../validators/images";
+import { validate } from "../validators";
 import { NotFoundError } from "../errors";
 
 const router = Router();
 
 // Get all images
-router.get("/", images.list);
+router.get("/", validate.pagination, validateRequest, images.list);
 
 // Upload images
 router.post("/create", upload.single("image"), images.create);
 
 // Get an image by id
-router.get("/:id", validateId, validateRequest, images.retrieve);
+router.get("/:id", validate.id, validateRequest, images.retrieve);
 
 // Update images
 router.patch(
   "/:id",
-  validateId,
+  validate.id,
   validateRequest,
   upload.single("image"),
   images.update
 );
 
 // Delete images
-router.delete("/:id", validateId, validateRequest, images.destroy);
+router.delete("/:id", validate.id, validateRequest, images.destroy);
 
 // Returns a 404 for not found routes
 router.all("*", async (req: Request, res: Response) => {
--- src/validators/images.ts
+++ src/validators/images.ts
@@ -1,6 +1,10 @@
-import { param } from "express-validator";
+import { param, query } from "express-validator";
 
 // Validate the id
-export const validateId = [
-  param("id").isUUID().withMessage("Must provide a valid id"),
+export const id = [param("id").isUUID().withMessage("Must provide a valid id")];
+
+// Validate pagination
+export const pagination = [
+  query("limit").optional().isInt().withMessage("Limit must be a valid number"),
+  query("page").optional().isInt().withMessage("Page must be a valid number"),
 ];

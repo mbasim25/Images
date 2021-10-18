e1d6dfd55550b8c6e2aabf9615b3688692e67cdc
mbasim25 <mohammedbasim2525@gmail.com>
2021-10-18 12:06:32 +0300

    Added: list & create routes

--- src/routers/images.ts
+++ src/routers/images.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { images } from "../controllers/";
+import { upload } from "../utils/";
 
 const router = Router();
+
+// Get all images
+router.get("/", images.list);
+
+// Upload images
+router.post("/create", upload.single("image"), images.create);
 
 export default router;

5e6c320c2ac64c1f99337e719de3eef62c353480
mbasim25 <mohammedbasim2525@gmail.com>
2021-10-19 11:26:06 +0300

    Extra heroku deployment settings

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -8,7 +8,6 @@ dotenv.config({
 
 const PORT = process.env.PORT || 8080;
 const DB_URL = process.env.DB_URL;
-const BASE = process.env.BASE;
-const BASE_URL = BASE! + ":" + PORT + "/";
+const BASE_URL = process.env.BASE_URL;
 
 export { PORT, DB_URL, BASE_URL };

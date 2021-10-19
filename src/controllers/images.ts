41f8c826285a0130afc0a61b98172b8fdf05748c
mbasim25 <mohammedbasim2525@gmail.com>
2021-10-19 10:21:12 +0300

    Added: pagination, Replaced: urlify w sql concat

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -1,31 +1,34 @@
 import { Request, Response } from "express";
 import { v4 } from "uuid";
-import ImageHandler from "../utils/image-handler";
+import { ImageHandler } from "../utils";
 import { pool } from "../../db";
 import { QueryResult } from "pg";
+import { BASE_URL } from "../utils/secrets";
+import { Query } from "../types";
 
 // Get all images
 export const list = async (req: Request, res: Response) => {
   try {
     // Pagination
-    const limit: any = req.query.limit || 20;
-    const page: any = req.query.page || 1;
+    const query: Query = req.query;
+    const limit = query.limit || 20;
+    const page = query.page || 1;
     const offset = (page - 1) * limit;
 
+    // Get all images and manipulate cover and thumbnail
     const results: QueryResult = await pool.query(
-      `SELECT * FROM images OFFSET $1 LIMIT $2 ;`,
-      [offset, limit]
+      `SELECT *,
+       CONCAT($1 :: VARCHAR,cover) AS cover,
+       CONCAT($1,thumbnail) AS thumbnail
+       FROM images OFFSET $2 LIMIT $3;`,
+      [BASE_URL, offset, limit]
     );
-
-    // Return images urls
-    for (const row of results.rows) {
-      await ImageHandler.urlify(row);
-    }
 
     const images = results.rows;
 
     return res.status(200).json(images);
   } catch (e) {
+    console.log(e);
     return res.status(400).json(e);
   }
 };
@@ -34,14 +37,21 @@ export const list = async (req: Request, res: Response) => {
 export const retrieve = async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
-    const image = await pool.query(`SELECT * FROM images WHERE id = $1;`, [id]);
+    const image = await pool.query(
+      `SELECT *,
+       CONCAT($1 :: VARCHAR,cover) AS cover,
+       CONCAT($1,thumbnail) AS thumbnail
+       FROM images WHERE id = $2`,
+      [BASE_URL, id]
+    );
 
     if (!image.rows[0]) {
       return res.status(404).json("Image not found");
     }
 
-    return res.status(200).json(await ImageHandler.urlify(image.rows[0]));
+    return res.status(200).json(image.rows[0]);
   } catch (e) {
+    console.log(e);
     return res.status(400).json(e);
   }
 };
@@ -59,17 +69,18 @@ export const create = async (req: Request, res: Response) => {
     const processed = await ImageHandler.process(req, id);
     const { cover, thumbnail } = processed!;
 
-    // Save to DB
-    await pool.query(
+    // Save to DB and return the values after concatenation
+    const results = await pool.query(
       `INSERT INTO images(id, cover, thumbnail) 
-       VALUES ($1, $2, $3);`,
-      [id, cover, thumbnail]
+       VALUES ($1, $2, $3) RETURNING *, 
+       CONCAT($4 :: VARCHAR,cover) AS cover,
+       CONCAT($4,thumbnail) AS thumbnail;`,
+      [id, cover, thumbnail, BASE_URL]
     );
 
-    // Return images urls
-    const images = await ImageHandler.urlify({ id, cover, thumbnail });
+    const image = results.rows[0];
 
-    return res.status(201).json(images);
+    return res.status(201).json(image);
   } catch (e) {
     return res.status(400).json(e);
   }
@@ -85,31 +96,33 @@ export const update = async (req: Request, res: Response) => {
     }
 
     // Check that the image exists
-    const unique = await pool.query(`SELECT * FROM images WHERE id = $1;`, [
+    const { rows } = await pool.query(`SELECT * FROM images WHERE id = $1;`, [
       id,
     ]);
 
-    if (!unique.rows[0]) {
+    if (!rows[0]) {
       return res.status(404).json("Image not found");
     }
 
     // Remove the old images
-    await ImageHandler.remove(unique);
+    await ImageHandler.remove(rows[0]);
 
     // Process the new images
     const processed = await ImageHandler.process(req, id);
     const { cover, thumbnail } = processed!;
 
-    // Save to DB
+    // Update, Save to DB, Return results after concatenation
     const results = await pool.query(
-      `UPDATE images SET cover = $1, thumbnail = $2 WHERE id = $3 RETURNING * ;`,
-      [cover, thumbnail, id]
+      `UPDATE images SET cover = $1, thumbnail = $2 
+       WHERE id = $3 RETURNING *, 
+       CONCAT($4 :: VARCHAR,cover) AS cover,
+       CONCAT($4,thumbnail) AS thumbnail; ;`,
+      [cover, thumbnail, id, BASE_URL]
     );
 
-    // Return images urls
-    const images = await ImageHandler.urlify(results.rows[0]);
+    const image = results.rows[0];
 
-    return res.status(200).json(images);
+    return res.status(200).json(image);
   } catch (e) {
     return res.status(400).json(e);
   }
@@ -121,14 +134,16 @@ export const destroy = async (req: Request, res: Response) => {
     const id = req.params.id;
 
     // Check that the image exists
-    const image = await pool.query(`SELECT * FROM images WHERE id = $1;`, [id]);
+    const { rows } = await pool.query(`SELECT * FROM images WHERE id = $1;`, [
+      id,
+    ]);
 
-    if (!image.rows[0]) {
+    if (!rows[0]) {
       return res.status(404).json("Image not found");
     }
 
     // Remove the images
-    await ImageHandler.remove(image);
+    await ImageHandler.remove(rows[0]);
 
     // Delete from DB
     await pool.query(`DELETE FROM images WHERE id = $1;`, [id]);
